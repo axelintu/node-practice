593992e09c5764718b10ab8d2a8e6881ef8bad67
Alejandro Flores <darklintumon@gmail.com>
2026-01-18 16:52:41 -0600

    Add inquierer library and prompt for name and favorite color, print msg in that color.

--- practica02Node/app.mjs
+++ practica02Node/app.mjs
@@ -1,8 +1,38 @@
-import chalk from 'chalk';
+import chalk from "chalk";
 import saludar from "./saludo.js";
+import { input, select } from "@inquirer/prompts";
 
-const name = process.argv[2];
 const log = console.log;
-const chk = chalk;
+const argName = process.argv[2];
+const argColor = process.argv[3];
+const colorChoices = {
+    message: "¿Cuál es tu color favorito?",
+    choices: [
+        {
+            name: "Rojo",
+            value: "red",
+        },
+        {
+            name: "Verde",
+            value: "green",
+        },
+        {
+            name: "Azul",
+            value: "blue",
+        },
+    ],
+};
 
-log(chk.blue(saludar( name ? name : 'Inadaptado')));
+const name = argName ? argName : await input({ message: "¿Cómo te llamas?" });
+const argColorObj = colorChoices.choices.find((col) => col.name === argColor);
+
+const colorValue = argColorObj
+    ? argColorObj.value
+    : await select(colorChoices);
+const selectedColor = colorChoices.choices.find(
+    (choice) => choice.value === colorValue,
+);
+const chk = chalk[colorValue];
+
+log(saludar(name));
+log(`${chk("Tu color favorito es " + selectedColor.name)}`);
