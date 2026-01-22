237f99be3aaf17e54c8be6698246bb395c28abbf
Alejandro Flores <darklintumon@gmail.com>
2026-01-22 00:31:40 -0600

    Update index.js for less repetition. Fixed 404 CSS

--- practica04-basic-backend/index.js
+++ practica04-basic-backend/index.js
@@ -3,48 +3,32 @@ import fs from "fs";
 import colors from "colors";
 import dayjs from "dayjs";
 
-const typeHTML = {"Content-type": "text/html"};
-const typeCSS  = {"Content-type": "text/css"};
+const cthtml = {"Content-type": "text/html"};
+const ctcss  = {"Content-type": "text/css"};
+const urls = [
+    {url: "/",           src: "index.html",    type: cthtml },
+    {url: "/nosotros",   src: "nosotros.html", type: cthtml },
+    {url: "/contacto",   src: "contacto.html", type: cthtml },
+    {url: "/styles.css", src: "styles.css",    type: ctcss  }
+]
 
 const server = http.createServer((req, res)=>{
-    switch (req.url) {
-        case "/":
-            const html = fs.readFileSync("./index.html","utf-8");
-            res.writeHead(typeHTML);
-            res.end(html);
-            console.log(colors.green(`[${dayjs().format()}] GET /` ));
-            break;
-        case "/nosotros":
-            res.writeHead(typeHTML);
-            res.end(fs.readFileSync(`./${req.url}.html`,"utf-8"));
-            console.log(colors.green(`[${dayjs().format()}] GET ${req.url}` ));
-            break;
-        case "/styles.css":
-            res.writeHead(200,typeCSS);
-            res.end(fs.readFileSync(`./styles.css`,"utf-8"));
-            console.log(colors.green(`[${dayjs().format()}] GET ${req.url}` ));
-            break;
-        case "/contacto":
-            res.writeHead(typeHTML);
-            res.end(fs.readFileSync(`./${req.url}.html`,"utf-8"));
-            console.log(colors.green(`[${dayjs().format()}] GET ${req.url}` ));
-            break
-        default:
-            const html404 = fs.readFileSync("./404.html","utf-8");
-            res.writeHead(404, {"content-type":"text/html"});
-            res.end(html404);
-            break;
+    const requestedUrl = req.url;
+    const found = urls.find((url)=> {
+        return url.url === requestedUrl;
+    });
+
+    if (found) {
+        const html = fs.readFileSync(`./${found.src}`,"utf-8");
+        res.writeHead(200, found.type);
+        res.end(html);
+        console.log(colors.green(`[${dayjs().format()}] GET ${requestedUrl}` ));
+    } else {
+        const html404 = fs.readFileSync("./404.html","utf-8");
+        res.writeHead(404, cthtml);
+        res.end(html404);
+        console.log(colors.red(`[${dayjs().format()}] GET ${requestedUrl}` ));
     }
-    // if (req.url === "/") {
-    //     const html = fs.readFileSync("./index.html","utf-8");
-    //     res.writeHead(200,{"Content-type": "text/html"});
-    //     res.end(html);
-    //     console.log(colors.green(`[${dayjs().format()}] GET /` ));
-    // } else {
-    //     const html = fs.readFileSync("./404.html","utf-8");
-    //     res.writeHead(404, {"content-type":"text/html"});
-    //     res.end(html);
-    // }
 });
 
 server.listen(3000, ()=> {
