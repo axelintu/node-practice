a44a487eec64be4dad3a00b33b7d2d35913770cc
Alejandro Flores <darklintumon@gmail.com>
2026-01-28 19:13:50 -0600

    Solve challenge 5.

---
+++ practica08-apis/index.js
@@ -0,0 +1,27 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+    const {method, url } = req;
+    res.writeHead(200, { "Content-Type": "application/json" });
+    switch (method) {
+        case 'GET':
+            res.end(JSON.stringify({ message: "Obteniendo lista de tareas..." }))
+        break;
+        case 'POST':
+            res.end(JSON.stringify({ message: "Creando una nueva tarea..." }))
+        break;
+        case 'PUT':
+            res.end(JSON.stringify({ message: "Actualizando una tarea existente..." }))
+        break;
+        case 'DELETE':
+            res.end(JSON.stringify({ message: "Tarea eliminada correctamente." }))
+        break;   
+        default:
+            res.end(JSON.stringify({ message: "Método no soportado" }))
+        break;
+    }
+});
+
+server.listen(3000,()=>{
+    console.log("Servidor escuchando...");
+});
