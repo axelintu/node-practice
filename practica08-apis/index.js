a531d1c14d98d164371efa17676b885c5e19bfe6
Alejandro Flores <darklintumon@gmail.com>
2026-01-28 19:48:09 -0600

    Update spacing on practica08.

--- practica08-apis/index.js
+++ practica08-apis/index.js
@@ -5,19 +5,29 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     switch (method) {
         case 'GET':
-            res.end(JSON.stringify({ message: "Obteniendo lista de tareas..." }))
+            res.end(JSON.stringify({ 
+                message: "Obteniendo lista de tareas..." 
+            }))
         break;
         case 'POST':
-            res.end(JSON.stringify({ message: "Creando una nueva tarea..." }))
+            res.end(JSON.stringify({ 
+                message: "Creando una nueva tarea..." 
+            }))
         break;
         case 'PUT':
-            res.end(JSON.stringify({ message: "Actualizando una tarea existente..." }))
+            res.end(JSON.stringify({ 
+                message: "Actualizando una tarea existente..." 
+            }))
         break;
         case 'DELETE':
-            res.end(JSON.stringify({ message: "Tarea eliminada correctamente." }))
+            res.end(JSON.stringify({ 
+                message: "Tarea eliminada correctamente." 
+            }))
         break;   
         default:
-            res.end(JSON.stringify({ message: "Método no soportado" }))
+            res.end(JSON.stringify({ 
+                message: "Método no soportado" 
+            }))
         break;
     }
 });
