d281b4fc855cc0e39b3059d12b4d788890aa4836
Alejandro Flores <darklintumon@gmail.com>
2026-02-03 20:51:12 -0600

    Solve api-express practice.

--- practica09-api-express/index.js
+++ practica09-api-express/index.js
@@ -2,67 +2,53 @@
 import express from "express";
 import colors from "colors";
 
-// 2. CREA LA INSTANCIA DE LA APLICACIÓN (app)
 const app = express();
-
-// 3. NUESTRA BASE DE DATOS FICTICIA
 let tasks = [
   { id: 1, desc: "Estudiar Node.js", status: "done" },
   { id: 2, desc: "Hacer ejercicio", status: "pending" },
   { id: 3, desc: "Leer documentación", status: "done" },
 ];
-
-// 4. MIDDLEWARE (Importante para que funcionen los POST)
-// Esto permite que tu servidor entienda cuando le envían datos en formato JSON
 app.use(express.json());
 
 
 // --- RUTAS ---
-
 // RUTA 1: GET (Obtener todas las tareas)
 // URL: /api/tasks
 app.get("/api/tasks", (req, res) => {
     // RESPONDE CON EL CÓDIGO 200 Y EL ARREGLO DE TAREAS (tasks)
     // Pista: usa res.status(200).json(...)
+    res.status(200).json(tasks);
 });
 
 // RUTA 2: GET (Obtener una tarea por su ID)
 // URL: /api/tasks/:id
 app.get("/api/tasks/:id", (req, res) => {
-    // A. OBTENER EL ID DE LOS PARÁMETROS (req.params.id) Y CONVERTIRLO A NÚMERO
-    
-    // B. BUSCAR LA TAREA EN EL ARREGLO (Usa un ciclo for o el método .find)
+    const id = parseInt(req.params.id);
     let task = null;
-    // ...tu lógica de búsqueda aquí...
-
-    // C. VALIDAR: SI NO EXISTE LA TAREA
-    if (!task) {
-         // Retorna un status 404 con un json que diga { error: "tarea no encontrada" }
+    const isTaskId = (task) => {
+        return task.id === id;
     }
-
-    // D. SI EXISTE, RESPONDE CON STATUS 200 Y LA TAREA ENCONTRADA
+    task = tasks.find(isTaskId);
+    if (!task) {
+        res.status(404).json( { error: "Tarea no encontrada" });
+    }
+    res.status(200).json(task);
 });
 
 // RUTA 3: POST (Crear una nueva tarea)
 // URL: /api/tasks
 app.post("/api/tasks", (req, res) => {
-    // A. DESESTRUCTURAR LA DESCRIPCIÓN (desc) DEL CUERPO DE LA PETICIÓN (req.body)
-    
-    // B. VALIDAR: SI NO HAY DESCRIPCIÓN
+    const { desc } = req.body;
     if (!desc) {
-        // Retorna status 400 con error: "description is required"
+        res.status(400).json({ error: "Description is required."});
     }
-
-    // C. CREAR EL NUEVO OBJETO TAREA
-    // El ID debe ser el del último elemento + 1
-    // El status inicial debe ser "pending"
     const newTask = {
-        // ...completa el objeto
+        id: tasks[tasks.length-1].id + 1,
+        desc: desc,
+        status: "pending"
     };
-
-    // D. AGREGAR LA NUEVA TAREA AL ARREGLO (push)
-    
-    // E. RESPONDER CON STATUS 201 (CREATED) Y EL ARREGLO ACTUALIZADO
+    tasks.push(newTask)
+    res.status(201).json(tasks);
 });
 
 
